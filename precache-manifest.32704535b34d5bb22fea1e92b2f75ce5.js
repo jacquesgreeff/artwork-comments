fa38750eb82f43e4fe444399eb2f29aecad36a9e
Jacques Greeff <jacques@realhub.com.au>
2020-06-24 14:47:50 +1000

    Updates

---
+++ precache-manifest.32704535b34d5bb22fea1e92b2f75ce5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6f438bafa8494193dc6f5aad892bbc5",
+    "url": "/artwork-comments/index.html"
+  },
+  {
+    "revision": "fa7b85902ed9889d7a4a",
+    "url": "/artwork-comments/static/css/main.7e22e123.chunk.css"
+  },
+  {
+    "revision": "0fbd5bf81f03b4a59521",
+    "url": "/artwork-comments/static/js/2.5847b67b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/artwork-comments/static/js/2.5847b67b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa7b85902ed9889d7a4a",
+    "url": "/artwork-comments/static/js/main.29e2bbc3.chunk.js"
+  },
+  {
+    "revision": "980883383c18fe4fa9f9",
+    "url": "/artwork-comments/static/js/runtime-main.c692c974.js"
+  }
+]);
